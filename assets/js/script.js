4b99337622dc99d089cac258758f4d019b281961
Rishav <vatsarishav99@gmail.com>
2023-08-06 20:18:37 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,23 +37,7 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
-    // <!-- emailjs to mail contact form data -->
-    //$("#contact-form").submit(function (event) {
-      /*  emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
-
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
-    });*/
-    // <!-- emailjs to mail contact form data -->
-
+    
 });
 
 document.addEventListener('visibilitychange',
@@ -71,7 +55,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Student", "NSS Volunteer", "web developer"],
+    strings: ["Student", "NSS Coordinator", "Web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
