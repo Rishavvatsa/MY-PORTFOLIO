ed7588894535cd35118de654618c91d7018a4629
Rishav <vatsarishav99@gmail.com>
2024-02-12 03:48:08 +0530

    new

--- assets/js/link.js
+++
@@ -1,4 +0,0 @@
-function resume()
-{
-   var resume= window.open("https://drive.google.com/file/d/1XlpTNbrWK7xcNUOAO4G4AhpJJaco4Sw5/view?usp=sharing")
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -55,7 +55,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Student", "NSS Coordinator", "Web developer"],
+    strings: ["Undergrad Student", "NSS Coordinator", "Web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
